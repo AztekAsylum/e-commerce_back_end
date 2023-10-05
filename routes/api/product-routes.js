54cc6aa49f70fc6d315b5197d07c4b96109a8fb2
AztekAsylum <mateo.rodriguez85@gmail.com>
2023-10-05 17:14:01 -0600

    updated ReadMe and walk-through video

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -122,8 +122,18 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteId = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deleteId);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
